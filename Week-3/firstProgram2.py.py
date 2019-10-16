d1c38e983dd7978b7a0fbcb73982bff6ceeef986
Kim Tae Eun <tae.e.kim19@imperial.ac.uk>
2019-10-16 23:21:15 +0100

    three guesses

---
+++ Week-3/firstProgram2.py.py
@@ -0,0 +1,26 @@
+print("what is your name?")
+name = input("Enter name:")
+print('hi ' + name)
+print ("Guess a number between 1 and 10")
+guess = input("Enter number:")
+guess = int(guess)
+import random
+y = random.randint(1,10)
+if y == guess:
+    print("Hurrah! the answer was " + str(y))
+else:
+    print("No match, try again")
+    guess = input("Enter number:")
+    guess = int(guess)
+    if y == guess:
+        print("Hurrah!! the answer was " + str(y))
+    else:
+        print("One last time")
+        guess = input("Enter number:")
+        guess = int(guess)
+        if y == guess:
+            print("Hurrah!!!")
+        else:
+            print("give up loser. the answer was " + str(y))
+        
+
