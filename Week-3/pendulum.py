446a4abcf068f8ef62950271c779b9d6a0d1d5f6
Kim Tae Eun <tae.e.kim19@imperial.ac.uk>
2019-10-22 11:37:36 +0100

    toys

---
+++ Week-3/pendulum.py
@@ -0,0 +1,9 @@
+def period(L, g,):
+    import math
+    T = 2*math.pi*(L/g)**1/2
+    return T
+
+
+
+
+
---
+++ Week-3/toysedit.py
@@ -0,0 +1,78 @@
+'''
+toys.py
+Simple toy functions to get comfortable working 
+with functions.
+'''
+
+
+# write a function that adds 1
+# to the input and prints the result
+def inc(a):
+    print(int(a) + 1)
+inc(input("enter number: "))
+
+
+# write a function that adds 1
+# to the input and returns the result
+def inc_return(a):
+    return int(a) + 1
+inc(input("enter number: "))  # hint this is incomplete
+
+
+# write a function that adds
+# the two input numbers together
+# and returns the sum
+
+def sum(a, b):
+    sum = a + b
+    return sum
+x = int(input("nom 1: "))
+y = int(input("nom 2: "))
+nom = sum(x, y)
+
+print(nom)
+
+
+
+# write a function that takes two
+# numbers, adds them together using 
+# sum() and then increments the sum
+# using inc_return
+def sum_inc(a, b):
+    sum_inc = a + b + 1
+    return sum_inc
+x = int(input("nom 1 : "))
+y = int(input("nom 2 : "))
+nom = sum_inc(x, y)
+print(nom)
+
+
+# write a function that returns a 
+# boolean (True or False) for whether 
+# the input number is even
+def is_even(a):
+    if a % 2 == 0:
+       return True
+    else:
+        return False
+b = int(input("enter number: "))
+ans = is_even(b)
+print(ans)
+
+
+# create for loop that takes a string
+# and an integer and returns a new string 
+# that is the string repeated equal to 
+# integer
+# e.g. string_repeat('ho', 3) returns
+# 'hohoho'
+def string_repeat(phrase, repeat):
+    string_repeat = phrase * int(repeat)
+    # hint: you can add strings together 
+    # in order to concatenate them
+    return string_repeat
+x = input("gimme a phrase: ")
+y = int(input("repeat: "))
+ans = string_repeat(x, y)
+print(ans)
+
