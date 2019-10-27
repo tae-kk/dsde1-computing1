53d2cef269ba4b638ed8c07f7b2b6150445966d9
Kim Tae Eun <tae.e.kim19@imperial.ac.uk>
2019-10-27 14:29:53 +0000

    2 errors

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,7 +1,18 @@
-def period(L, g,):
+def period(L, g):
     import math
-    T = 2*math.pi*(L/g)**1/2
-    return T
+    T = 2 * math.pi*math.sqrt(L/g)
+    if g == 0:
+        raise ZeroDivisionError("g cannot be zero")
+    if isinstance(L, str) == True:
+        raise TypeError ("L is a number")
+    if L < 0:
+        raise ValueError ("L is a positive value")
+    if isinstance(g, str) == True:
+        raise TypeError ("g is a number") 
+    if g < 0:
+        raise ValueError ("g is greater than 0")
+    else:
+        return T
 
 
 
