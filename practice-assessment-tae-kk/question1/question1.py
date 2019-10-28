b38e88651fd64373044f5e0202d9310b438b52e7
Kim Tae Eun <tae.e.kim19@imperial.ac.uk>
2019-10-28 19:58:10 +0000

    answer1

---
+++ practice-assessment-tae-kk/question1/question1.py
@@ -0,0 +1,23 @@
+'''
+question1.py
+
+Implementation of the flowchart in question1.png.
+'''
+
+def flowchart(input_value):
+    if input_value == 0:
+        return "zero"
+    elif input_value > 0:
+        if input_value % 2 == 0:
+           return "positive-even"
+        else:
+            return "positive-odd"
+    elif input_value < 0:
+        if input_value % 2 == 0:
+            return "negative-even"
+        else:
+            return "negative-odd"
+
+
+
+
